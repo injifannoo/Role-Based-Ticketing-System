b5197c42e53423ce802e6bb18bf7373c2010b1b0
injifannoo <injifanotu@gmail.com>
2025-03-06 16:06:08 +0300

    I set up middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+exports.authenticateUser = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+exports.authorizeAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+  next();
+};
